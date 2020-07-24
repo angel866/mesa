6319d136b9a55762e60b50bf933380f67f3a3a1e
angel866 <67845519+angel866@users.noreply.github.com>
2020-07-24 01:50:26 -0500

    Add files via upload

---
+++ paginas/sweetalert.js
@@ -0,0 +1,18 @@
+
+
+
+Swal.fire({
+	title:'I.E.P 20130 JESUS REY',
+	
+confirmButtonText:'CERRAR',
+with: '100%',
+imageUrl:'img/COMU.jpg',
+imageAlt:'foto',
+backdrop:'true',
+allowOutsideClick:false,
+allowEscapekey:false,
+allowEnterkey:true,
+stopkeydownPropagation:false,
+}
+	);
+
