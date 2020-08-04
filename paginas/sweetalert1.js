31cbb56926f81ea9624e2cfbf58283f2a4e6ab41
angel866 <67845519+angel866@users.noreply.github.com>
2020-08-04 00:21:53 -0500

    Add files via upload

---
+++ paginas/sweetalert1.js
@@ -0,0 +1,21 @@
+
+
+
+(async () => {
+
+const { value: password } = await Swal.fire({
+  title: 'Enter your password',
+  input: 'password',
+  inputPlaceholder: 'Enter your password',
+  inputAttributes: {
+    maxlength: 10,
+    autocapitalize: 'off',
+    autocorrect: 'off',
+  }
+})
+
+if (password) {
+  Swal.fire('Entered password: ${password}')
+}
+
+});
