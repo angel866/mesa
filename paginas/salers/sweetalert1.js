d9031df80d8e8bbfea9fbdf80dd7a5cc63959516
angel866 <67845519+angel866@users.noreply.github.com>
2020-09-04 12:34:33 -0500

    Update sweetalert1.js

--- paginas/salers/sweetalert1.js
+++ paginas/salers/sweetalert1.js
@@ -5,7 +5,7 @@ Swal.fire({
 	title:'I.E.P 20130 JESUS REY',
 confirmButtonText:'CERRAR',
 with: '100%',
-imageUrl:'imagen/titulo1.jpg',
+imageUrl:'imagen/titulo6.jpg',
 width : '700 %',
 padding:0,
 backdrop:'true',
